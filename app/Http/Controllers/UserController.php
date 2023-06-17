7c6ca5e2aee65e292dc3396fa1c3d80ba3d977aa
Win Barua <win.barua2@gmail.com>
2023-06-17 16:56:00 +0100

    Add pagination and add page titles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -87,7 +87,7 @@ class UserController extends Controller
     public function showCorrectHomepage(Request $request)
     {
         if (auth()->check()) {
-            return view('homepage-feed', ['posts' => $request->user()->feedPosts()->latest()->get()]);
+            return view('homepage-feed', ['posts' => $request->user()->feedPosts()->latest()->paginate(2)]);
         } else {
             return view('homepage');
         }
