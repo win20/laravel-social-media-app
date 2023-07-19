6ed5a1bdd157878d7b4098b51fe3d2750bded103
Win Barua <win.barua2@gmail.com>
2023-07-19 22:35:50 +0100

    Setup broadcasting with Pusher

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Events\ChatMessage;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\FollowController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,3 +50,23 @@ Route::get('/profile/{user:username}/following', [UserController::class, 'profil
 
 Route::post('/create-follow/{user:username}', [FollowController::class, 'createFollow'])->middleware('mustBeLoggedIn');
 Route::post('/remove-follow/{user:username}', [FollowController::class, 'removeFollow'])->middleware('mustBeLoggedIn');
+
+// Chat route
+Route::post('/send-chat-message', function (Request $request) {
+    $formFields = $request->validate([
+        'textvalue' => 'required',
+    ]);
+
+    if (!trim(strip_tags($formFields['textvalue']))) {
+        return response()->noContent();
+    }
+
+    broadcast(new ChatMessage(
+        [
+            'username' => auth()->user()->username,
+            'textvalue' => strip_tags($request->textvalue),
+            'avatar' => auth()->user()->avatar
+        ]
+    ))->toOthers();
+    return response()->noContent();
+})->middleware('mustBeLoggedIn');
